42c226c684f2b57de8e9506b2e65135b57499b9c
Yash jain <97658674+Realcod@users.noreply.github.com>
2024-05-26 06:48:43 +0000

    new version..

--- script.js
+++ script.js
@@ -1,267 +1,84 @@
 async function payWithMetamask() {
   if (typeof web3 !== 'undefined') {
-      web3 = new Web3(web3.currentProvider);
+    web3 = new Web3(web3.currentProvider);
 
-      try {
-          const accounts = await web3.eth.requestAccounts();
-          console.log(accounts) // Prompt the user to connect their Metamask wallet
-          const userAddress = accounts[0];
+    try {
+      const accounts = await web3.eth.requestAccounts();
+      console.log(accounts); // Prompt the user to connect their Metamask wallet
+      const userAddress = accounts[0];
 
-          const tokenAddress = '0xef61Eb508DD2da8Ed460E3496191e61afa916565'; // Replace with your ERC20 token contract address
-          const tokenAbi = [ 
-      {
-          "inputs": [
-              {
-                  "internalType": "uint256",
-                  "name": "total",
-                  "type": "uint256"
-              }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-      },
-      {
-          "anonymous": false,
-          "inputs": [
-              {
-                  "indexed": true,
-                  "internalType": "address",
-                  "name": "tokenOwner",
-                  "type": "address"
-              },
-              {
-                  "indexed": true,
-                  "internalType": "address",
-                  "name": "spender",
-                  "type": "address"
-              },
-              {
-                  "indexed": false,
-                  "internalType": "uint256",
-                  "name": "tokens",
-                  "type": "uint256"
-              }
-          ],
-          "name": "Approval",
-          "type": "event"
-      },
-      {
-          "anonymous": false,
-          "inputs": [
-              {
-                  "indexed": true,
-                  "internalType": "address",
-                  "name": "from",
-                  "type": "address"
-              },
-              {
-                  "indexed": true,
-                  "internalType": "address",
-                  "name": "to",
-                  "type": "address"
-              },
-              {
-                  "indexed": false,
-                  "internalType": "uint256",
-                  "name": "tokens",
-                  "type": "uint256"
-              }
-          ],
-          "name": "Transfer",
-          "type": "event"
-      },
-      {
-          "inputs": [
-              {
-                  "internalType": "address",
-                  "name": "owner",
-                  "type": "address"
-              },
-              {
-                  "internalType": "address",
-                  "name": "delegate",
-                  "type": "address"
-              }
-          ],
-          "name": "allowance",
-          "outputs": [
-              {
-                  "internalType": "uint256",
-                  "name": "",
-                  "type": "uint256"
-              }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-      },
-      {
-          "inputs": [
-              {
-                  "internalType": "address",
-                  "name": "delegate",
-                  "type": "address"
-              },
-              {
-                  "internalType": "uint256",
-                  "name": "numTokens",
-                  "type": "uint256"
-              }
-          ],
-          "name": "approve",
-          "outputs": [
-              {
-                  "internalType": "bool",
-                  "name": "",
-                  "type": "bool"
-              }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-      },
-      {
-          "inputs": [
-              {
-                  "internalType": "address",
-                  "name": "tokenOwner",
-                  "type": "address"
-              }
-          ],
-          "name": "balanceOf",
-          "outputs": [
-              {
-                  "internalType": "uint256",
-                  "name": "",
-                  "type": "uint256"
-              }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-      },
-      {
-          "inputs": [],
-          "name": "decimals",
-          "outputs": [
-              {
-                  "internalType": "uint8",
-                  "name": "",
-                  "type": "uint8"
-              }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-      },
-      {
-          "inputs": [],
-          "name": "name",
-          "outputs": [
-              {
-                  "internalType": "string",
-                  "name": "",
-                  "type": "string"
-              }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-      },
-      {
-          "inputs": [],
-          "name": "symbol",
-          "outputs": [
-              {
-                  "internalType": "string",
-                  "name": "",
-                  "type": "string"
-              }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-      },
-      {
-          "inputs": [],
-          "name": "totalSupply",
-          "outputs": [
-              {
-                  "internalType": "uint256",
-                  "name": "",
-                  "type": "uint256"
-              }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-      },
-      {
-          "inputs": [
-              {
-                  "internalType": "address",
-                  "name": "receiver",
-                  "type": "address"
-              },
-              {
-                  "internalType": "uint256",
-                  "name": "numTokens",
-                  "type": "uint256"
-              }
-          ],
-          "name": "transfer",
-          "outputs": [
-              {
-                  "internalType": "bool",
-                  "name": "",
-                  "type": "bool"
-              }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-      },
-      {
-          "inputs": [
-              {
-                  "internalType": "address",
-                  "name": "owner",
-                  "type": "address"
-              },
-              {
-                  "internalType": "address",
-                  "name": "buyer",
-                  "type": "address"
-              },
-              {
-                  "internalType": "uint256",
-                  "name": "numTokens",
-                  "type": "uint256"
-              }
-          ],
-          "name": "transferFrom",
-          "outputs": [
-              {
-                  "internalType": "bool",
-                  "name": "",
-                  "type": "bool"
-              }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-      }
-   ];
+      const tokenAddress = '0xef61Eb508DD2da8Ed460E3496191e61afa916565'; // Replace with your ERC20 token contract address
+      const tokenAbi = [ 
+        // Your ABI JSON here
+      ];
 
-          const tokenContract = new web3.eth.Contract(tokenAbi, tokenAddress);
-          const amount = web3.utils.toWei('100', 'ether'); // Convert 100 tokens to wei
+      const tokenContract = new web3.eth.Contract(tokenAbi, tokenAddress);
+      const amount = web3.utils.toWei('100', 'ether'); // Convert 100 tokens to wei
 
-          // Call the transfer function of your ERC20 token contract
-          const receipt = await tokenContract.methods.transfer('0x0dd1f70265Af6eBd50f6aB05AB35bE53dc08634E', amount)
-              .send({ from: userAddress });
+      // Call the transfer function of your ERC20 token contract
+      const receipt = await tokenContract.methods.transfer('0x0dd1f70265Af6eBd50f6aB05AB35bE53dc08634E', amount)
+        .send({ from: userAddress });
 
-          console.log("Transaction receipt: ", receipt);
-          alert("Transaction successful")
-
-
-      } catch (error) {
-          console.error("Error: ", error);
-      }
+      console.log("Transaction receipt: ", receipt);
+      alert("Transaction successful");
+    } catch (error) {
+      console.error("Error: ", error);
+    }
   } else {
-      console.error("Metamask is not installed.");
+    console.error("Metamask is not installed.");
   }
-
 }
 
 document.getElementById("payButton").addEventListener("click", payWithMetamask);
+
+function updateThingSpeakManually(fieldValue) {
+  const apiKey = "IXX3CR9AYZH9XQOW";
+  let url = "";
+  if (fieldValue === 1) {
+    url = `https://api.thingspeak.com/update?api_key=${apiKey}&field3=1`;
+  } else if (fieldValue === 0) {
+    url = `https://api.thingspeak.com/update?api_key=${apiKey}&field3=0`;
+  }
+  window.open(url, '_blank'); // Open URL in a new tab
+}
+
+// Speech recognition functionality
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+if (SpeechRecognition) {
+  const recognition = new SpeechRecognition();
+
+  recognition.continuous = false;
+  recognition.interimResults = false;
+  recognition.lang = 'en-US';
+
+  recognition.onstart = function() {
+    console.log('Speech recognition started');
+  };
+
+  recognition.onspeechend = function() {
+    console.log('Speech recognition ended');
+    recognition.stop();
+  };
+
+  recognition.onresult = function(event) {
+    const transcript = event.results[0][0].transcript.toLowerCase();
+    console.log('Transcript: ', transcript);
+    alert(`You said: ${transcript}`);
+
+    if (transcript.includes("turn on")) {
+      updateThingSpeakManually(1);
+    } else if (transcript.includes("turn off")) {
+      updateThingSpeakManually(0);
+    }
+  };
+
+  recognition.onerror = function(event) {
+    console.error('Speech recognition error: ', event.error);
+  };
+
+  document.querySelector('.btn.medium').addEventListener('click', function() {
+    recognition.start();
+  });
+} else {
+  console.error('Web Speech API is not supported by this browser.');
+}
